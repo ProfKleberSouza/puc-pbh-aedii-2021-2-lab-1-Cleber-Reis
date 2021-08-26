5b93163485476be1ebf685c7aea09d0ba0f12205
Cleber  Reis <61522744+Cleber-Reis@users.noreply.github.com>
2021-08-26 22:02:49 +0000

    Exercício 09 em construção

--- exe09/main.c
+++ exe09/main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
-int main() {
-
-    int a, b, s;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
-  return(0);
+int main() 
+{
+    placar_possiveis(3, 1);
+    
+   return(0);
 }
